147e3a19f1eac566a24e3b20fe0d0633acfda0dc
Aleksandr Danilenko <rorik302@gmail.com>
2024-01-31 22:53:30 +0300

    Установлены ssl сертификаты, хост

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from "node:url"
 
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
+import { readFileSync } from "fs"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,5 +11,12 @@ export default defineConfig({
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url))
     }
+  },
+  server: {
+    https: {
+      cert: readFileSync("./certs/cert.pem"),
+      key: readFileSync("./certs/key.pem")
+    },
+    host: "localhost.com"
   }
 })
